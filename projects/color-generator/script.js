579822beecdafe994453845379a3231a6beb4336
Galastique <anthokess@hotmail.com>
2022-07-14 11:05:06 -0400

    compressed code

--- projects/color-generator/script.js
+++ projects/color-generator/script.js
@@ -1,50 +1,37 @@
 //Changes background color and text fields
 function changeCSS(){
-    document.getElementById("noJS").innerHTML = "";
     let colors = generate();
-    let rgb = getRGB(colors);
+    let rgb = `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
     let hex = getHex(colors);
-
     document.body.style.backgroundColor = rgb;
-
     document.getElementById("rgb").innerHTML = rgb.toUpperCase();
     document.getElementById("hex").innerHTML = hex.toUpperCase();
 }
 
 //Generates random color
 function generate(){
-    let colors = [];
-
+    let c = [];
     for(let i = 0; i < 3; i++){
-        colors.push(Math.floor(Math.random() * 256));
+        c.push(Math.floor(Math.random() * 256));
     }
-
-    return colors;
-}
-
-//Gets RGB value
-function getRGB(colors){
-    return `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+    return c;
 }
 
 //Gets hex value
 function getHex(colors){
-    let red = colors[0].toString(16);
-    let green = colors[1].toString(16);
-    let blue = colors[2].toString(16);
-    let hex = "#";
-
-    if(red.length == 1){
-        red = `0${red}`;
+    let r = colors[0].toString(16);
+    let g = colors[1].toString(16);
+    let b = colors[2].toString(16);
+    let h = "#";
+    if(r.length == 1){
+        r = `0${r}`;
     }
-    if(green.length == 1){
-        green = `0${green}`;
+    if(g.length == 1){
+        g = `0${g}`;
     }
-    if(blue.length == 1){
-        blue = `0${blue}`;
+    if(b.length == 1){
+        b = `0${b}`;
     }
-
-    hex += red + green + blue;
-
-    return hex;
+    h += r + g + b;
+    return h;
 }
