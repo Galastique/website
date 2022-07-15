c77f87b2b1965c901c6aebc76eb3b62dbf252733
Galastique <anthokess@hotmail.com>
2022-07-15 09:58:13 -0400

    adjusted color

--- projects/reaction-test/script.js
+++ projects/reaction-test/script.js
@@ -40,7 +40,7 @@ function start(){
 
 //Counts reaction time
 function test(){
-    document.body.style.backgroundColor = "RGB(80, 200, 80)";
+    document.body.style.backgroundColor = "RGB(100, 180, 100)";
     document.getElementById("title").innerHTML = "Click!";
     ongoing = "yes";
     startTime = Date.now();
