71f81182929a5a78faebb815c71a5e136dae33b0
Galastique <anthokess@hotmail.com>
2023-04-03 17:27:59 -0400

    this should fix it for real now

--- scripts/getJsonData.js
+++ scripts/getJsonData.js
@@ -1,22 +1,8 @@
-import projects from "../data/projects.json" assert {type: "json"};
-import logs from "../data/logs.json" assert {type: "json"};
-import hangman from "../data/hangman.json" assert {type: "json"};
-
-const dataList = [
-    ["projects", "logs", "hangman"],
-    [projects, logs, hangman]
-];
-
 export function getJSON(fileName) {
-    let isFirefox = typeof InstallTrigger !== 'undefined';
-
-    if(isFirefox){
-        //I KNOW THIS METHOD IS DEPRECATED, BUT FOR SOME REASON THE BETTER, SHORTER METHOD DOESN'T WORK ON FIREFOX
-        let request = new XMLHttpRequest();
-        request.open("GET", `../data/${fileName}.json`, false);
-        request.send(null)
-        return JSON.parse(request.responseText);
-    }else{
-        return dataList[1][dataList[0].indexOf(fileName)];
-    }
+    //I KNOW THIS METHOD IS DEPRECATED, BUT FOR SOME REASON THE BETTER, SHORTER METHOD DOESN'T WORK ON FIREFOX
+    //otherwise I would just do     import projects from "../data/projects.json" assert {type: "json"};
+    let request = new XMLHttpRequest();
+    request.open("GET", `../data/${fileName}.json`, false);
+    request.send(null)
+    return JSON.parse(request.responseText);
 }
