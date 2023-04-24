a281add430527a502812f9b13a2ffd0b229f8729
Galastique <anthokess@hotmail.com>
2023-04-24 14:34:35 -0400

    added text mocker mini-project

---
+++ scripts/projects/text-mocker.js
@@ -0,0 +1,19 @@
+//Changes properties
+function mock(){
+    let text = document.getElementById("encryptionMessage").value;
+    let newText = mockify(text);
+    document.getElementById("decryptionMessage").innerText = newText;
+    text != "" && document.getElementById("decryptionMessage").removeAttribute("disabled");
+}
+
+//Generates message
+function mockify(text){
+    let newText = "";
+    for(let char of text){
+        if(isNaN(char)){
+            Math.floor(Math.random() * 2) == 1 ? char = char.toLowerCase() : char = char.toUpperCase();
+        }
+        newText += char;
+    }
+    return newText;
+}
