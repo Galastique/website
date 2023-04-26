f5a4a7a30f970e518eae2c1a877ea59a8977c414
Galastique <anthokess@hotmail.com>
2023-04-26 16:18:36 -0400

    removed useless lines of code

--- scripts/projects/text-mocker.js
+++ scripts/projects/text-mocker.js
@@ -1,18 +1,15 @@
 //Changes properties
 function mock(){
     let text = document.getElementById("encryptionMessage").value;
-    let newText = mockify(text);
-    document.getElementById("decryptionMessage").value = newText;
+    document.getElementById("decryptionMessage").value = mockify(text);
     text != "" && document.getElementById("decryptionMessage").removeAttribute("disabled");
 }
 
-//Generates message
+//Generates mocking message
 function mockify(text){
     let newText = "";
     for(let char of text){
-        if(isNaN(char)){
-            Math.floor(Math.random() * 2) == 1 ? char = char.toLowerCase() : char = char.toUpperCase();
-        }
+        Math.floor(Math.random() * 2) == 1 ? char = char.toLowerCase() : char = char.toUpperCase();
         newText += char;
     }
     return newText;
