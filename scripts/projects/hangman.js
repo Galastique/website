474fec179f907e0c1f1bd63308dc9361f5162a98
Galastique <anthokess@hotmail.com>
2023-06-30 12:03:43 -0400

    removed laggy block of useless code

--- scripts/projects/hangman.js
+++ scripts/projects/hangman.js
@@ -2,15 +2,3 @@ import {wordList} from "../../data/hangman.js"
 let word = wordList[Math.floor(Math.random() * wordList.length)];
 
 console.log(`Wordlist contains ${wordList.length} words`);
-checkDuplicates();
-
-//Checks for duplicates and counts
-function checkDuplicates(){
-    for (let i = 0; i < wordList.length; i++) {
-        for (let j = 0; j < wordList.length; j++) {
-            if (wordList[i] == wordList[j] && i != j) {
-                console.log(`"${wordList[i]}" is a duplicate at word ${i}`);
-            }
-        }
-    }
-}
