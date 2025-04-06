0707544a04fc9bd2b6e8b19217c9fae344fbfbb2
Galastique <anthokess@hotmail.com>
2025-04-05 21:07:11 -0400

    Fixed link

--- data/portfolio.js
+++ data/portfolio.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         name: "Hoptop",
         hover: "Hoptop: A reinvented buy and sell platform!",
-        link: "www.hoptop.app",
+        link: "https://www.hoptop.app",
         role: "Lead backend developer",
         description: "Hoptop is a web app that was created to reinvent the way people look for professional services. End customers can create posts when they need professional services done (for example roofing or landscaping), which will notify the different companies in the area that cover that specific domain, who can then each send their own offers and availabilities. The customer can then easily compare prices and choose who they want to accomplish the job.<br><br>In the project, I developed the entirety of the API on my own. I handled the database scheme, authentication, security, validations, API integrations, websockets, error handling, cookie management and everything else in between. When I started the project, I didn't know much about how to build an API. I learned everything on my own by Googling and doing my own little experiments.<br><br>I also had to take care of deploying everything on the Google Cloud Platform.",
         image: "hoptop",
